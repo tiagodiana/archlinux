81dcdf7bed6c2995b674da8bedc72718ded7a942
tiagodiana <tiago.diana@etec.sp.gov.br>
2020-01-13 12:16:07 -0300

    scripts pos instalação

---
+++ react-native_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Instalando react Native
+# Instalando NodeJs
+
+echo "Instalando NodeJs e NPM"
+sudo pacman -S nodejs npm
+
+# Instalando React Native CLI
+echo "Instalando React Native CLI"
+sudo npm install -g react-native-cli
+
+# Instalando JDK
+echo "Instalando JDK8"
+sudo pacman -S jdk8-openjdk
+echo "Setando Java 8 no sistema"
+sudo archlinux-java set java-8-openjdk
+
+# Criando pasa para skd android
+echo "Criando pastas para Android SDK"
+mkdir -p ~/Android/Sdk
+cd ~/Android/Sdk
+echo "Baixando SDK"
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+echo "Extraindo SDK"
+unzip sdk-tools-linux-4333796.zip
+
+echo "Copiando export para o bashrc"
+echo "export ANDROID_HOME=~/Android/Sdk" >> ~/.bashrc
+echo "export PATH=${PATH}:${ANDROID_HOME}/tools" >> ~/.bashrc
+echo "export PATH=${PATH}:${ANDROID_HOME}/platform-tools" >> ~/.bashrc
+
+~/Android/Sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-27" "build-tools;27.0.3"
+
